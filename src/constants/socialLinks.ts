0980aacd93bda23a55780e415ed5330dabcbbbe8
AnujAcharjee <anujacharjee37@gmail.com>
2026-03-02 18:27:17 +0000

    removed linked in link

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -1,7 +1,7 @@
 export const SOCIAL_LINKS = [
-    { icon: 'bi bi-github', url: 'https://github.com/AnujAcharjee' },
-    { icon: 'bi bi-linkedin', url: 'https://linkedin.com/in/AnujAcharjee' },
-    { icon: 'bi bi-twitter-x', url: 'https://x.com/anuj_400' },
-    { icon: 'bi bi-instagram', url: null },
-    { icon: 'bi bi-file-earmark-person', url: null },
-  ];
+  { icon: 'bi bi-github', url: 'https://github.com/AnujAcharjee' },
+  { icon: 'bi bi-twitter-x', url: 'https://x.com/anuj_400' },
+  { icon: 'bi bi-linkedin', url: null },
+  { icon: 'bi bi-instagram', url: null },
+  { icon: 'bi bi-file-earmark-person', url: null },
+];
